81846f21380e6863342911966fbb77109c7c3223
Fabio Penteado <flzpenteado@gmail.com>
2020-02-19 06:49:36 -0300

    User tests now uses db

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,16 +1,15 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('It should list all users', () => request(app).get('/users')
     .then(res => {
         expect(res.status).toBe(200);
-        expect(res.body).toHaveLength(1);
-        expect(res.body[0]).toHaveProperty('name', 'John Doe');
+        expect(res.body.length).toBeGreaterThan(0);
     }));
 
 
 test('It should correctly insert an user', () => request(app).post('/users')
-    .send({ name: 'Walter White', email: 'walter@email.com' })
+    .send({ name: 'Walter White', email: `${Date.now()}@email.com`, password: 'pwd123' })
     .then(res => {
         expect(res.status).toBe(201);
         expect(res.body.name).toBe('Walter White');
