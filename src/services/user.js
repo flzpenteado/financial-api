7d1f7d60df377cc0e9225b3d2477d7879e6288a4
Fabio Penteado <flzpenteado@gmail.com>
2020-02-20 08:14:50 -0300

    Added first validations on service layer

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,17 @@
 module.exports = app => {
-    const findAll = () => app.db('users').select();
+    const findAll = (filter = {}) => app.db('users').select().where(filter);
 
-    const save = user => app.db('users').insert(user, '*');
+    const save = async user => {
+        if (!user.name) return { error: 'Name is a required field' };
+        if (!user.email) return { error: 'Email is a required field' };
+        if (!user.password) return { error: 'Password is a required field' };
+
+        const existingUser = await findAll({ email: user.email }).first();
+
+        if (existingUser) return { error: 'Email already exists' };
+
+        return app.db('users').insert(user, '*');
+    };
 
     return { findAll, save };
 };
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
+const email = `${Date.now()}@email.com`;
+
 test('It should list all users', () => request(app).get('/users')
     .then(res => {
         expect(res.status).toBe(200);
@@ -9,8 +11,35 @@ test('It should list all users', () => request(app).get('/users')
 
 
 test('It should correctly insert an user', () => request(app).post('/users')
-    .send({ name: 'Walter White', email: `${Date.now()}@email.com`, password: 'pwd123' })
+    .send({ name: 'Walter White', email, password: 'pwd123' })
     .then(res => {
         expect(res.status).toBe(201);
         expect(res.body.name).toBe('Walter White');
+    }));
+
+test('It should not insert an user without name property', () => request(app).post('/users')
+    .send({ email: `${Date.now()}@email.com`, password: 'pwd123' })
+    .then(res => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Name is a required field');
+    }));
+
+test('It should not insert an user without email property', () => request(app).post('/users')
+    .send({ name: 'Walter White', password: 'pwd123' })
+    .then(res => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Email is a required field');
+    }));
+
+test('It should not insert an user without password property', () => request(app).post('/users')
+    .send({ name: 'Walter White', email: `${Date.now()}@email.com` })
+    .then(res => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Password is a required field');
+    }));
+
+test('It should not insert an user with an existing email', () => request(app).post('/users')
+    .send({ name: 'Walter White', email, password: 'pwd123' })
+    .then(res => {
+        expect(res.body.error).toBe('Email already exists');
     }));
